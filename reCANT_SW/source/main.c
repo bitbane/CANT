9a61327d23f916cb21971a9a3b7338eba8ef2a54
Tim Brom <thb@grimm-co.com>
2022-07-22 14:52:45 -0400

    Building and linking with correct(?) startup and linker script

--- reCANT_SW/source/main.c
+++ reCANT_SW/source/main.c
@@ -3,36 +3,22 @@
  */
 
 #include "fsl_device_registers.h"
-#include "fsl_debug_console.h"
 #include "clock_config.h"
 
-#include "usb_device_config.h"
-#include "usb.h"
-#include "usb_device.h"
-#include "usb_phy.h"
 #include "peripherals.h"
 #include "pin_mux.h"
-
-#define CONTROLLER_ID 2u
 
 /*!
  * @brief Main function
  */
 int main(void)
 {
-    char ch;
-
     /* Init board hardware. */
     BOARD_InitBootPeripherals();
     BOARD_InitBootClocks();
     BOARD_InitBootPins();
-    DbgConsole_Init((uint8_t)CONTROLLER_ID, (uint32_t)NULL, kSerialPort_UsbCdc, (uint32_t)NULL);
-
-    PRINTF("hello world.\r\n");
 
     while (1)
     {
-        ch = GETCHAR();
-        PUTCHAR(ch);
     }
 }
