2d5297efd290115fbf9a5123834497c829352ac1
Tim Brom <thb@grimm-co.com>
2022-07-22 23:14:53 -0400

    Works as long as main does not call BOARD_InitBootPeripherals

--- reCANT_SW/source/main.c
+++ reCANT_SW/source/main.c
@@ -2,6 +2,10 @@
  * Copyright (c) 2022 - 2022, Tim Brom
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "fsl_device_registers.h"
 #include "clock_config.h"
 
@@ -14,11 +18,23 @@
 int main(void)
 {
     /* Init board hardware. */
-    BOARD_InitBootPeripherals();
     BOARD_InitBootClocks();
+    SystemCoreClockUpdate();
     BOARD_InitBootPins();
+    BOARD_InitBootPeripherals();
+    uint32_t count = 0;
 
     while (1)
     {
+        count++;
+
+        if(count == 200000000)
+            GPIO_PinWrite(BOARD_INITPINS_TEENSY_LED_PORT, BOARD_INITPINS_TEENSY_LED_PIN, 0);
+
+        if(count >= 400000000)
+        {
+            GPIO_PinWrite(BOARD_INITPINS_TEENSY_LED_PORT, BOARD_INITPINS_TEENSY_LED_PIN, 1);
+            count = 0;
+        }
     }
 }
