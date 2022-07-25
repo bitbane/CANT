6b1d0f415d00a6e30f832a55a363778705fdf65a
Tim Brom <thb@grimm-co.com>
2022-07-24 22:39:59 -0400

    Change the number of flashes based on the selected program

--- reCANT_SW/source/main.c
+++ reCANT_SW/source/main.c
@@ -16,6 +16,7 @@
 #include "usb.h"
 #include "test.h"
 
+uint32_t num_flashes = 1;
 
 /*!
  * @brief Main function
@@ -39,37 +40,44 @@ int main(void)
     if(GPIO_PinRead(BOARD_INITPINS_RUN_CANCAT_PORT, BOARD_INITPINS_RUN_CANCAT_PIN))
     {
         strcpy(program, "CanCat\r\n");
+        num_flashes = 1;
     }
     else if(GPIO_PinRead(BOARD_INITPINS_RUN_CANT_PORT, BOARD_INITPINS_RUN_CANT_PIN))
     {
         strcpy(program, "CANT\r\n");
+        num_flashes = 2;
     }
     else if(GPIO_PinRead(BOARD_INITPINS_RUN_SOCKETCAN_PORT, BOARD_INITPINS_RUN_SOCKETCAN_PIN))
     {
         strcpy(program, "SocketCAN\r\n");
+        num_flashes = 3;
     }
     else if(GPIO_PinRead(BOARD_INITPINS_RUN_TRUCKDEVIL_PORT, BOARD_INITPINS_RUN_TRUCKDEVIL_PIN))
     {
         strcpy(program, "TruckDevil\r\n");
+        num_flashes = 4;
     }
     else if(GPIO_PinRead(BOARD_INITPINS_RUN_SW5_PORT, BOARD_INITPINS_RUN_SW5_PIN))
     {
         strcpy(program, "SW5\r\n");
+        num_flashes = 5;
     }
     else if(GPIO_PinRead(BOARD_INITPINS_RUN_SW6_PORT, BOARD_INITPINS_RUN_SW6_PIN))
     {
         strcpy(program, "SW6\r\n");
+        num_flashes = 6;
     }
     else if(GPIO_PinRead(BOARD_INITPINS_RUN_SW7_PORT, BOARD_INITPINS_RUN_SW7_PIN))
     {
         strcpy(program, "SW7\r\n");
+        num_flashes = 7;
     }
     else
     {
         strcpy(program, "TEST\r\n");
+        num_flashes = UINT32_MAX;
         runTestProgram();
     }
-
 
     while (1)
     {
@@ -78,11 +86,18 @@ int main(void)
 
 /* Interrupts */
 volatile uint32_t system_ticks = 0;
+volatile uint32_t flash_count = 0;
 void SysTick_Handler(void)
 {
-    if((system_ticks & 0x00000FFF) == 0x800)
-        GPIO_PinWrite(BOARD_INITPINS_TEENSY_LED_PORT, BOARD_INITPINS_TEENSY_LED_PIN, 1);
-    else if ((system_ticks & 0x00000FFF) == 0x000)
+    if((system_ticks & 0x000001FF) == 0x100)
+    {
+        flash_count++;
+        if(flash_count <= num_flashes)
+            GPIO_PinWrite(BOARD_INITPINS_TEENSY_LED_PORT, BOARD_INITPINS_TEENSY_LED_PIN, 1);
+        else
+            flash_count = 0;
+    }
+    else if ((system_ticks & 0x000001FF) == 0x000)
         GPIO_PinWrite(BOARD_INITPINS_TEENSY_LED_PORT, BOARD_INITPINS_TEENSY_LED_PIN, 0);
     system_ticks++;
     tud_task();
